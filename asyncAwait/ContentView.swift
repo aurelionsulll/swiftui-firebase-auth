24eec1616a2c4cb03e71b75868ed603fd5dd04a9
aurelionsulll <mennanamehdi@gmail.com>
2021-10-10 12:38:56 +0100

    Initial Commit

---
+++ asyncAwait/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  asyncAwait
+//
+//  Created by mehdi on 10/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ asyncAwait/asyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncAwaitApp.swift
+//  asyncAwait
+//
+//  Created by mehdi on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct asyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
